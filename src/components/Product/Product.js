98228944caffec97950dd51cf5d4016fe7ec2f37
Sohidul Islam <sishufol.sim@gmail.com>
2022-05-29 16:54:40 +0600

    Display dynamic star ratings on products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import "./Product.css"
+import Rating from 'react-rating';
 // const element = <FontAwesomeIcon icon={faCoffee} />
 const Product = (props) => {
     // console.log(props);
-    const { id, name, price, seller, img, stock } = props.product;
+    const { id, name, price, seller, img, stock, ratings } = props.product;
     return (
         <div className="product slit-in-vertical">
             <img src={img} alt="" />
@@ -13,8 +14,14 @@ const Product = (props) => {
                 <h2>{name}</h2>
                 <p><small>by: {seller}</small></p>
                 <p>by: ${price}</p>
-                <p><small>only {stock} left in stock - order soon</small></p>
-                <button onClick={() => props.handleAddtoCart(props.product)} className="btn-regular">{<FontAwesomeIcon icon={faCartShopping} style={{ marginRight: "8px" }} />}add to cart</button>
+                <p><small>only {stock} left in stock - order soon rating: {ratings}</small></p>
+
+                <Rating onChange={(value) => console.log("rating: ", value)} emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color" initialRating={ratings} readonly ></Rating>
+                <br></br>
+                <button onClick={() => props.handleAddtoCart(props.product)} className="btn-regular">{<FontAwesomeIcon icon={faCartShopping} style={{
+                    marginRight: "8px"
+                }} />}add to cart</button>
             </div>
 
         </div>
